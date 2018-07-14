dfa9bf6f96db5931db1650f01d3209cad8ea5a86
Paavan Chopra <paavan012chopra@gmail.com>
2018-07-14 19:03:12 -0400

    firs commit

---
+++ src/main/java/wiki/link/hop/HopApplication.java
@@ -0,0 +1,12 @@
+package wiki.link.hop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopApplication.class, args);
+	}
+}
---
+++ src/test/java/wiki/link/hop/HopApplicationTests.java
@@ -0,0 +1,16 @@
+package wiki.link.hop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
