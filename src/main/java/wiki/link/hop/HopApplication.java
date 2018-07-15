b857179f6dea82243b8ebf9376604717a4074e69
Paavan Chopra <paavan012chopra@gmail.com>
2018-07-14 21:40:59 -0400

    save running app

--- src/main/java/wiki/link/hop/HopApplication.java
+++ src/main/java/wiki/link/hop/HopApplication.java
@@ -1,12 +1,19 @@
 package wiki.link.hop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.map.repository.config.EnableMapRepositories;
 
 @SpringBootApplication
+@EnableMapRepositories
 public class HopApplication {
+	private static final Logger logger = LoggerFactory.getLogger(HopApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(HopApplication.class, args);
+		ConfigurableApplicationContext cxt = SpringApplication.run(HopApplication.class, args);
+		logger.info("Running on port: " + cxt.getEnvironment().getProperty("local.server.port"));
 	}
 }
